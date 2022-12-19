5d48547a29ac0e073da4124340de130afade5687
Jholman Daniel Reina Toledo <reina.daniel1005@gmail.com>
2022-12-19 13:24:57 -0500

    Correccion

--- tablas_historicos_terraes.py
+++ tablas_historicos_terraes.py
@@ -3,7 +3,7 @@ import os
 def generar_triggers(schema, tablass):
     file = open("tabla_historico_terraes.txt", "w")
     for tabla in tablass:
-        estructura = 'create table sghistorico.historico' + tabla + '(\n'
+        estructura = 'create table sg_historico.historico_' + tabla + '(\n'
         estructura = estructura + ' like ' + schema + '.' + tabla + ',\n'
         estructura = estructura + ' fecha_modificacion timestamptz not null default now(),\n'
         estructura = estructura + ' usuario_modificacion bigint not null default sg_ciudadano.current_user_id() references sg_administracion.usuario(id),\n'
@@ -12,24 +12,28 @@ def generar_triggers(schema, tablass):
         estructura = estructura + ' resolucion_modificante bigint not null default sg_administracion.current_user_resolucion() references sg_administracion.resolucion(id)\n'
         estructura = estructura + ' );\n'
         estructura = estructura + '\n'
-        estructura = estructura + ' create index on sghistorico.historico' + tabla +'(usuario_modificacion);\n'
-        estructura = estructura + ' create index on sghistorico.historico' + tabla +'(operacion_ultima_modificacion);\n'
-        estructura = estructura + ' create index on sghistorico.historico' + tabla +'(resolucion_modificante);\n'
+        estructura = estructura + ' create index on sg_historico.historico_' + tabla +'(usuario_modificacion);\n'
+        estructura = estructura + ' create index on sg_historico.historico_' + tabla +'(operacion_ultima_modificacion);\n'
+        estructura = estructura + ' create index on sg_historico.historico_' + tabla +'(resolucion_modificante);\n'
         file.write(estructura + os.linesep)
     file.close()
 
 schema = 'sg_catastro'
 
 tablas = [
-    'zona_fisica_predio',
-    'zona_geoeconomica_predio',
-    'lindero_predio',
-    'av_tablacalificacionconstruccion',
-    'av_tipologiaconstruccion',
-    'av_zonahomogeneafisicarural',
-    'av_zonahomogeneafisicaurbana',
-    'av_zonahomogeneageoeconomicarural',
-    'av_zonahomogeneageoeconomicaurbana',
+    'cc_barrio',
+    'cc_centropoblado',
+    'cc_corregimiento',
+    'cc_limitemunicipio',
+    'cc_localidadcomuna',
+    'cc_manzana',
+    'cc_metodooperacion',
+    'cc_nomenclaturavial',
+    'cc_nomenclaturavial_tipo_via',
+    'cc_perimetrourbano',
+    'cc_sectorrural',
+    'cc_sectorurbano',
+    'cc_vereda',
 ]
 
 generar_triggers(schema, tablas)
--- trigger_historicos_terraes.py
+++ trigger_historicos_terraes.py
@@ -3,9 +3,9 @@ import os
 def generar_triggers(tablass):
     file = open("triggers_historicos_terrates.txt", "w")
     for tabla in tablass:
-        estructura = 'create or replace function sghistorico.historico' + tabla + '_funcion() returns trigger as $$\n'
+        estructura = 'create or replace function sg_historico.historico_' + tabla + '_funcion() returns trigger as $$\n'
         estructura = estructura + 'begin\n'
-        estructura = estructura + '  insert into sghistorico.historico' + tabla + '(\n'
+        estructura = estructura + '  insert into sg_historico.historico_' + tabla + '(\n'
         estructura = estructura + '    t_id,\n'
         estructura = estructura + '    operacion_ultima_modificacion\n'
         estructura = estructura + '  ) values (\n'
@@ -21,25 +21,29 @@ def generar_triggers(tablass):
         estructura = estructura + '$$ language plpgsql strict security definer;\n'
         estructura = estructura + '\n'
         estructura = estructura + '\n'
-        estructura = estructura + 'create trigger tgghistorico' + tabla + '\n'
+        estructura = estructura + 'create trigger tgg_historico_' + tabla + '\n'
         estructura = estructura + '  before update or delete\n'
         estructura = estructura + '  on sg_catastro.' + tabla + '\n'
         estructura = estructura + '  for each row\n'
-        estructura = estructura + '  execute procedure sghistorico.historico' + tabla + '_funcion();\n'
+        estructura = estructura + '  execute procedure sg_historico.historico_' + tabla + '_funcion();\n'
         estructura = estructura + '\n'
         estructura = estructura + '\n'
         file.write(estructura + os.linesep)
     file.close()
 
 tablas = [
-    'zona_fisica_predio',
-    'zona_geoeconomica_predio',
-    'lindero_predio',
-    'av_tablacalificacionconstruccion',
-    'av_tipologiaconstruccion',
-    'av_zonahomogeneafisicarural',
-    'av_zonahomogeneafisicaurbana',
-    'av_zonahomogeneageoeconomicarural',
-    'av_zonahomogeneageoeconomicaurbana',
+    'cc_barrio',
+    'cc_centropoblado',
+    'cc_corregimiento',
+    'cc_limitemunicipio',
+    'cc_localidadcomuna',
+    'cc_manzana',
+    'cc_metodooperacion',
+    'cc_nomenclaturavial',
+    'cc_nomenclaturavial_tipo_via',
+    'cc_perimetrourbano',
+    'cc_sectorrural',
+    'cc_sectorurbano',
+    'cc_vereda',
 ]
 generar_triggers(tablas)
